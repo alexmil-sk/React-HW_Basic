396d1597e36c48ea498e3e68db04c865316be7f8
alexmil-sk <alexmilweb@gmail.com>
2021-11-24 12:50:40 +0100

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './App.module.css';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className={classes.container}>
+				<h1>Main Title React App</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
