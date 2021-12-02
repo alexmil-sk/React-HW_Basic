ddf84f4ba620ae45133ea9e75c9495fc6db6dfcb
alexmil-sk <alexmilweb@gmail.com>
2021-12-02 19:17:10 +0100

    ready one chat

--- src/components/MsgForm/MsgForm.jsx
+++ src/components/MsgForm/MsgForm.jsx
@@ -37,6 +37,7 @@ function MsgForm(props) {
 			setMsgSubmit((msgSubmit) => {
 				const newMsgSubmit = [...msgSubmit];
 				newMsgSubmit.unshift({
+					idx: Date.now(),
 					id: valueId,
 					title: valueTitle,
 					body: valueBody,
@@ -55,19 +56,54 @@ function MsgForm(props) {
 		);
 	}
 
-	// useEffect(() => {
-	// 	const timer = setTimeout(() => {
-	// 		const arrKey = msgSubmit.map(item => item.id)
-	// 		props.callBot(arrKey[0]);
-	// 		//arrKey.map(key => props.callBot(key));
-	// 	}, 1500)
-	// 	return () => { clearTimeout(timer) };
-	// }, [msgSubmit])
+	useEffect(() => {
+
+		if (msgSubmit.length === 0) { return; }
+
+		const lastUser = msgSubmit.map(item => item.id)
+
+		if (lastUser[0] === `info-bot`) {
+			return;
+		}
+		const timer = setTimeout(() => {
+			if (lastUser[0] === 'user') {
+				console.log(lastUser[0]);
+				setMsgSubmit(() => {
+					const newMsgSubmit = [...msgSubmit];
+					newMsgSubmit.unshift({
+						idx: Date.now(),
+						id: 'info-bot',
+						title: 'I n f o B o t',
+						body: `Hi, ** ${lastUser[0]} ** I'm Botty around here!`,
+						image: 'https://st4.depositphotos.com/20523700/37870/v/1600/depositphotos_378706342-stock-illustration-vector-illustration-robot-icon.jpg'
+					})
+					return newMsgSubmit;
+				})
+				return;
+			} else {
+				setMsgSubmit(() => {
+					const newMsgSubmit = [...msgSubmit];
+					newMsgSubmit.unshift({
+						idx: Date.now(),
+						id: 'info-bot',
+						title: 'I n f o B o t',
+						body: `Hi, **${lastUser[0]}** You are not a HUMAN! Man, if you wonna be a HUMAN sign your message as "USER"`,
+						image: 'https://st4.depositphotos.com/20523700/37870/v/1600/depositphotos_378706342-stock-illustration-vector-illustration-robot-icon.jpg'
+					});
+					return newMsgSubmit;
+				})
+			}
+
+		}, 2000);
+		return () => { clearTimeout(timer) };
+
+	}, [msgSubmit]);
 
 
-	const delSubMsgs = useCallback((id) => {
+	const delSubMsgs = useCallback((idx) => {
+		console.log(idx);
 		setMsgSubmit((msgSubmit) => {
-			return msgSubmit.filter(user => user.id !== id);
+			return msgSubmit.filter(user => user.idx !== idx);
 		})
 	}, [])
 
@@ -76,53 +112,53 @@ function MsgForm(props) {
 	return (
 		<div className={classes.container}>
 			<h2 className="mainTitle">Message Form</h2>
-				<form onSubmit={onSubmitForm} className={classes.form}>
-						<input
-							className={classes.formElem}
-							type="text"
-							name="id"
-							onChange={onChangeId}
-							placeholder="User's name"
-							value={valueId}
-						/>
-						<input
-							className={classes.formElem}
-							type="text"
-							name="image"
-							onChange={onChangeImage}
-							placeholder="Users's avatar http://....."
-							value={valueImage}
-						/>
-						<input
-							className={classes.formElem}
-							type="text"
-							name="title"
-							onChange={onChangeTitle}
-							placeholder="Message's title"
-							value={valueTitle}
-						/>
-						<textarea
-							className={classes.formElem}
-							name="body"
-							cols="30"
-							rows="10"
-							onChange={onChangeBody}
-							placeholder="Message's text"
-							value={valueBody}
-						/>
-					<button className={classes.btn}>Send Message</button>
-				</form>
+			<form onSubmit={onSubmitForm} className={classes.form}>
+				<input
+					className={classes.formElem}
+					type="text"
+					name="id"
+					onChange={onChangeId}
+					placeholder="User's name"
+					value={valueId}
+				/>
+				<input
+					className={classes.formElem}
+					type="text"
+					name="image"
+					onChange={onChangeImage}
+					placeholder="Users's avatar http://....."
+					value={valueImage}
+				/>
+				<input
+					className={classes.formElem}
+					type="text"
+					name="title"
+					onChange={onChangeTitle}
+					placeholder="Message's title"
+					value={valueTitle}
+				/>
+				<textarea
+					className={classes.formElem}
+					name="body"
+					cols="30"
+					rows="10"
+					onChange={onChangeBody}
+					placeholder="Message's text"
+					value={valueBody}
+				/>
+				<button className={classes.btn}>Send Message</button>
+			</form>
 			<div className={classes.userMsgContainer}>
 				<div className={classes.userMsg}>
-					<TransitionGroup component="div">
-						{msgSubmit.map(msg => {
+					<TransitionGroup component="span">
+						{msgSubmit.map((msg) => {
 							return (
 								<CSSTransition
-									//key={msg.id}
+									key={msg.idx}
 									timeout={1000}
 									classNames="msgUseAnimated"
 								>
-									<div key={msg.id} className={classes.msgForm}>
+									<div key={msg.idx} className={classes.msgForm}>
 										<div className={classes.msgContent}>
 											<div className={classes.msgId}>
 												<img src={msg.image} alt={msg.id} className={classes.msgImg} />
@@ -134,7 +170,7 @@ function MsgForm(props) {
 											</div>
 											<button
 												className={classes.delBtn}
-												onClick={() => delSubMsgs(msg.id)}
+												onClick={() => delSubMsgs(msg.idx)}
 											>&times;</button>
 										</div>
 									</div>
