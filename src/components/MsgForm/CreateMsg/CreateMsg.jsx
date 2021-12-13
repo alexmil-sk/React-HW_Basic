be6689e912ad6ed645f8076c6ebf69b9943732d4
alexmil-sk <alexmilweb@gmail.com>
2021-12-13 13:02:49 +0100

    added CreateMsg to store... go for ListMsg

--- src/components/MsgForm/CreateMsg/CreateMsg.jsx
+++ src/components/MsgForm/CreateMsg/CreateMsg.jsx
@@ -6,134 +6,134 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { Redirect, useParams } from 'react-router-dom';
 import { TextField, Container, Box, Avatar, Typography, Button, Badge, ListItem, List } from '@mui/material';
 import MessageIcon from '@mui/icons-material/Message';
+import { getMsg } from '../../../store/msgForm/selectorsMsgForm.js';
 
 
 
-function MsgForm() {
+function CreateMsg({ onSave }) {
 
 	const { chatId } = useParams();//! Достает из match.params одну из его характеристик: postId
 
-	const [msgSubmit, setMsgSubmit] = useState([]);
+	const [msgValue, setMsgValue] = useState({
+		id: '',
+		title: '',
+		body: '',
+		image: ''
+	})
 
-	const [valueId, setValueId] = useState('');
-	const [valueTitle, setValueTitle] = useState('');
-	const [valueBody, setValueBody] = useState('');
-	const [valueImage, setValueImage] = useState('');
+	const msgReset = useCallback(() => {
+		setMsgValue({
+			id: '',
+			title: '',
+			body: '',
+			image: ''
+		})
+	}, []);
 
+	//todo_Ссылка для активного поля Id
 	const inputIdRef = useRef(null);
 
-
-
-	const onChangeId = (e) => {
-		setValueId(e.target.value);
-	}
-
-	const onChangeTitle = (e) => {
-		setValueTitle(e.target.value);
-	}
-
-	const onChangeBody = (e) => {
-		setValueBody(e.target.value);
-	}
-
-	const onChangeImage = (e) => {
-		setValueImage(e.target.value);
-	}
+	/**
+	 * @param {'id' | 'image' | 'title' | 'body'} name
+	 */
+	const getMsgValueByName = (name) => msgValue[name]
+	
+	const onChange = (name) =>
+		(e) => {
+			setMsgValue((prev) => ({
+				...prev,
+				[name]: e.target.value
+			}))
+		}
 
 	const onSubmitForm = (e) => {
 		e.preventDefault();
-		if (valueId && valueTitle && valueBody) {
-			setMsgSubmit((msgSubmit) => {
-				const newMsgSubmit = [...msgSubmit];
-				newMsgSubmit.unshift({
-					idx: Date.now(),
-					id: valueId,
-					title: valueTitle,
-					body: valueBody,
-					image: valueImage ? `${valueImage}` : 'https://cs8.pikabu.ru/avatars/1832/x1832143-2115011424.png'
-				})
-
-				return newMsgSubmit;
-			})
-		} else { alert('Заполните, пожалуйста, все поля сообщения!') }
-
-
-		return (
-			setValueId(''),
-			setValueTitle(''),
-			setValueBody(''),
-			setValueImage('')
-		);
+		if (msgValue.id === '' || msgValue.title === '' || msgValue.body === '') {
+			alert('Заполните поля');
+		}
+		onSave(msgValue);
+		msgReset();
 	}
 
-	useEffect(() => {
 
-		if (msgSubmit.length === 0) { return; }
+	//!_ACHTUNG!! Das ist Robot!========================
+	// useEffect(() => {
 
-		const lastUser = msgSubmit.map(item => item.id)
+		// if (msgSubmit.length === 0) { return; }
 
-		if (lastUser[0] === 'info-bot' || lastUser[0] === 'sckeleton') {
-			return;
-		}
-		const timer = setTimeout(() => {
-			if (lastUser[0] === 'user') {
-				setMsgSubmit(() => {
-					const newMsgSubmit = [...msgSubmit];
-					newMsgSubmit.unshift({
-						idx: Date.now(),
-						id: 'info-bot',
-						title: 'I n f o B o t',
-						body: `Hi, ** ${lastUser[0]} ** I'm Botty around here!`,
-						image: 'https://img2.freepng.ru/20180804/pso/kisspng-mobile-robot-stock-photography-image-illustration-%D8%AF%D8%B1%D8%A8%D8%A7%D8%B1%D9%87-%D9%85%D8%A7-%D8%B4%D8%B1%DA%A9%D8%AA-%D8%A2%D8%B1%D8%B3%D8%B3-5b65f6073773d3.7988520615334087752271.jpg'
-					})
-					return newMsgSubmit;
-				})
-				return;
-			} else if (lastUser[0] === '') {
-				return;
-			} else {
-				setMsgSubmit(() => {
-					const newMsgSubmit = [...msgSubmit];
-					newMsgSubmit.unshift({
-						idx: Date.now(),
-						id: 'sckeleton',
-						title: 'I n f o B o t',
-						body: `Hi, **${lastUser[0]}** You are not a HUMAN! Man, if you wonna be a HUMAN sign your message as "USER"`,
-						image: 'https://avatars.mds.yandex.net/i?id=d5cbec0c701f73502694dd515466cb5e-3070965-images-thumbs&ref=rim&n=33&w=225&h=225'
-					});
-					return newMsgSubmit;
-				})
-			}
+		// const lastUser = msgSubmit.map(item => item.id)
 
-		}, 2000);
-		return () => { clearTimeout(timer) };
+		// if (lastUser[0] === 'info-bot' || lastUser[0] === 'sckeleton') {
+			// return;
+		// }
+		// const timer = setTimeout(() => {
+			// if (lastUser[0] === 'user') {
+				// setMsgSubmit(() => {
+					// const newMsgSubmit = [...msgSubmit];
+					// newMsgSubmit.unshift({
+						// idx: Date.now(),
+						// id: 'info-bot',
+						// title: 'I n f o B o t',
+						// body: `Hi, ** ${lastUser[0]} ** I'm Botty around here!`,
+						// image: 'https://img2.freepng.ru/20180804/pso/kisspng-mobile-robot-stock-photography-image-illustration-%D8%AF%D8%B1%D8%A8%D8%A7%D8%B1%D9%87-%D9%85%D8%A7-%D8%B4%D8%B1%DA%A9%D8%AA-%D8%A2%D8%B1%D8%B3%D8%B3-5b65f6073773d3.7988520615334087752271.jpg'
+					// })
+					// return newMsgSubmit;
+				// })
+				// return;
+			// } else if (lastUser[0] === '') {
+				// return;
+			// } else {
+				// setMsgSubmit(() => {
+					// const newMsgSubmit = [...msgSubmit];
+					// newMsgSubmit.unshift({
+						// idx: Date.now(),
+						// id: 'sckeleton',
+						// title: 'I n f o B o t',
+						// body: `Hi, **${lastUser[0]}** You are not a HUMAN! Man, if you wonna be a HUMAN sign your message as "USER"`,
+						// image: 'https://avatars.mds.yandex.net/i?id=d5cbec0c701f73502694dd515466cb5e-3070965-images-thumbs&ref=rim&n=33&w=225&h=225'
+					// });
+					// return newMsgSubmit;
+				// })
+			// }
 
-	}, [msgSubmit]);
+		// }, 2000);
+		// return () => { clearTimeout(timer) };
+
+	// }, [msgSubmit]);
+
+	//!_Das ist den Robot ende!====================
+
+
+	//todo_useEffect для активного поля Id
 
 	useEffect(() => {
 		inputIdRef.current?.focus();
-	}, [valueId]);
+	}, [msgValue.id]);
+	//todo=============================
 
-	const delSubMsgs = useCallback((idx) => {
-		setMsgSubmit((msgSubmit) => {
-			return msgSubmit.filter(user => user.idx !== idx);
-		})
-	}, [])
+	//!_Delete Button for message=======
+	//const delSubMsgs = useCallback((idx) => {
+	//	setMsgValue((msgValue) => {
+	//		return msgValue.filter(user => user.idx !== idx);
+	//	})
+	//}, [])
 
-//*=================================================================
-	
+	//!_Delete Button for message end=======
+
+	//*=================================================================
+
 
 	if (!chatUsersArray.find(({ id }) => id === chatId)) {
 		return <Redirect to="/404" />;
 	}
 
-//*==================================================================
+	//*==================================================================
 
 
 	return (
 		<>
 			<Container sx={{ padding: '0!important', ml: 1, mr: 1 }}>
-				<Box sx={{mb: '25px' }}>
+				<Box sx={{ mb: '25px' }}>
 					<div className={classes.container}>
 						<Typography
 							color="secondary"
@@ -146,37 +146,35 @@ function MsgForm() {
 								className={classes.formElem}
 								type="text"
 								name="id"
-								onChange={onChangeId}
+								onChange={onChange('id')}
 								variant="outlined"
 								margin="dense"
 								color="secondary"
 								label="User's name"
-								value={valueId}
+								value={getMsgValueByName('id')}
 							/>
 							<TextField
 								className={classes.formElem}
 								type="text"
 								name="image"
-								onChange={onChangeImage}
+								onChange={onChange('image')}
 								variant="filled"
 								size="normal"
 								margin="dense"
-								//color="warning"
 								color="secondary"
 								label="Users's avatar http://....."
-								value={valueImage}
+								value={getMsgValueByName('image')}
 							/>
 							<TextField
 								className={classes.formElem}
 								type="text"
 								name="title"
-								onChange={onChangeTitle}
+								onChange={onChange('title')}
 								variant="outlined"
 								margin="dense"
-								//color="warning"
 								color="secondary"
 								label="Message's title"
-								value={valueTitle}
+								value={getMsgValueByName('title')}
 							/>
 							<TextField
 								multiline
@@ -186,30 +184,28 @@ function MsgForm() {
 								className={classes.formElem}
 								name="body"
 								rows={3}
-								onChange={onChangeBody}
+								onChange={onChange('body')}
 								label="Message's text"
 								placeholder="Message's text"
-								value={valueBody}
+								value={getMsgValueByName('body')}
 							/>
 							<Button
 								onClick={onSubmitForm}
 								variant="contained"
-								//endIcon={<SendIcon />}
 								className={classes.btn}
-								//color="info"
 								color="secondary"
 								size="large"
 								sx={{ width: 80, height: 80, margin: '15px auto', borderRadius: '50%', boxShadow: '0 0 10px 5px #07233E' }}
 							>
-								<Badge overlap="circular" color="info" sx={{ color: 'white' }} badgeContent={msgSubmit.length}>
+								<Badge overlap="circular" color="info" sx={{ color: 'white' }} badgeContent={getMsg.length}>
 									<MessageIcon sx={{ fontSize: 50 }} />
 								</Badge>
 							</Button>
 						</form>
-						<div className={classes.userMsgContainer}>
+						{/* <div className={classes.userMsgContainer}>
 							<List className={classes.userMsg}>
 								<TransitionGroup component="span">
-									{msgSubmit.map((msg) => {
+									{msgValue.map((msg) => {
 										return (
 											<CSSTransition
 												key={msg.idx}
@@ -246,7 +242,7 @@ function MsgForm() {
 									}
 								</TransitionGroup>
 							</List>
-						</div>
+						</div> */}
 					</div>
 				</Box>
 			</Container>
@@ -254,5 +250,5 @@ function MsgForm() {
 	)
 }
 
-export default MsgForm;
+export default CreateMsg;
 
--- src/components/MsgForm/MsgForm.jsx
+++ src/components/MsgForm/MsgForm.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import classes from './MsgForm.module.css';
 import './MsgFormAnime.css';
 import CreateMsg from './CreateMsg/CreateMsg';
+import { createMessage } from '../../store/msgForm/actionsMsgForm.js';
 
 
 
 function MsgForm() {
 
+	const dispatch = useDispatch();
+
+	const onSave = (value) => {
+		dispatch(createMessage({
+			id: Date.now(),
+			...value
+		}))
+	}
+
 	return (
 		<>
-			<CreateMsg />
+			<CreateMsg onSave={onSave}/>
 		</>
 	)
 }
