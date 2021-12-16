dba0d77b6e94e395e142a429854d8450568766d2
alexmil-sk <alexmilweb@gmail.com>
2021-12-16 11:07:19 +0100

    ...continue

--- src/components/MsgForm/MsgForm.jsx
+++ src/components/MsgForm/MsgForm.jsx
@@ -1,4 +1,4 @@
-import React, from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classes from './MsgForm.module.css';
 import './MsgFormAnime.css';
