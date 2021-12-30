aa072a7d731e19a35271ce24ca03b55be3c05429
alexmil-sk <alexmilweb@gmail.com>
2021-12-30 10:16:25 +0100

    createMsg(7) + reducerChats(5) + MsgForm(4) are passed

--- src/components/MsgForm/MsgForm.test.jsx
+++ src/components/MsgForm/MsgForm.test.jsx
@@ -6,15 +6,29 @@ import { create } from "react-test-renderer";
 
 
 
-describe('MsgForm component',() => {
-	it('MsgForm component renders with msgList', () => {
+describe('MsgForm component', () => {
+	it('MsgForm component have been rendered', () => {
+		const msgList = ['item1', 'item2'];
+		render(<MsgFormRender msgList={msgList} />);
+		expect(screen.getAllByText(/User's name/i)).toBeTruthy();
+	});
+	it('MsgList component have been rendered', () => {
+		const msgList = ['item1', 'item2'];
+		render(<MsgFormRender msgList={msgList} />);
+		expect(screen.getByText(/messages/i)).toBeInTheDocument();
+	});
+	it('MsgForm component get props msgList', () => {
 		const msgList = ['item1', 'item2'];
 		render(<MsgFormRender msgList={msgList}/>);
-		screen.debug();
 		expect(msgList.length).toBe(2);
+	});
+	it('MsgForm component get props onSave', () => {
+		const msgList = ['item1', 'item2'];
+		const onSave = jest.fn();
+		render(<MsgFormRender onSave={onSave} msgList={msgList}/>);
+		expect(onSave).toBeTruthy();
 	});
 
 
-	
 });
 
